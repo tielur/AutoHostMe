704e9397105c4db2eba616e0ab2526ef7af01962
Tyler Clemens <tyler.r.clemens@gmail.com>
2018-09-22 18:19:22 -0700

    Add log for when work happens

--- lib/periodically.ex
+++ lib/periodically.ex
@@ -14,11 +14,16 @@ defmodule Periodically do
 
   def handle_info(:work, state) do
     {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, _body}} = :httpc.request(:get, {'https://auto-host-me.herokuapp.com/', []}, [], [])
+    debug "Successfully pinged https://auto-host-me.herokuapp.com"
     schedule_work() # Reschedule once more
     {:noreply, state}
   end
 
   defp schedule_work() do
     Process.send_after(self(), :work, 15 * 60 * 1000) # In 15 mins
+  end
+
+  defp debug(msg) do
+    IO.puts(IO.ANSI.yellow() <> msg <> IO.ANSI.reset())
   end
 end
